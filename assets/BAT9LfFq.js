6bab3a2f4b4d476c5ad7589a6281d8d5bd85f82f
truongtop1 <pattlactroi@gmail.com>
2024-05-25 23:58:59 +0700

    Updates

---
+++ assets/BAT9LfFq.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DlAUqK2U.js";import{o as c,c as e}from"./sazWOm0d.js";const _={};function n(r,t){return c(),e("div")}const m=o(_,[["render",n]]);export{m as _};
--- dist/server/assets/tr-TR-BYkJKbF2.js
+++
@@ -1,9 +0,0 @@
-const resource = {
-  "hello": { "t": 0, "b": { "t": 2, "i": [{ "t": 3 }], "s": "merhaba" } },
-  "follow": { "t": 0, "b": { "t": 2, "i": [{ "t": 3 }], "s": "Takip Et" } },
-  "modal": { "t": 0, "b": { "t": 2, "i": [{ "t": 3 }], "s": "Ekran" } }
-};
-export {
-  resource as default
-};
-//# sourceMappingURL=tr-TR-BYkJKbF2.js.map
